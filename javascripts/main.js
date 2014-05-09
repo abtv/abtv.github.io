5fc0b0f903a04973067ed922cb0ddc03d3a0999b
Andrey Butov <andreybutov@mail.ru>
2014-05-09 23:15:05 +0400

    Little javascript mixin

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,21 @@
 console.log('This would be the main JS file.');
+
+function action(element){
+	var showed = element.attr("showed");
+	console.log(showed);
+	var anti = !showed;
+	console.log(anti);
+	element.attr("showed", anti);
+	console.log(element.attr("showed"));
+	if (showed === true){
+		element.hide();	
+	} else {
+		element.show();
+	}
+};
+
+$(document).ready(function(){
+	$("h3").next().hide();
+	$("h3").next().attr("showed", false);
+	$("h3").click(function(){ action($(this).next()); });
+});
