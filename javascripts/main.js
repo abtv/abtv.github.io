58802c40bda6c488417f0f0d9c5a3ed7d65085a9
Andrey Butov <andreybutov@mail.ru>
2014-05-11 17:28:31 +0400

    Hide and open items

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,21 +1,46 @@
-console.log('This would be the main JS file.');
+console.log('Start running...');
+
+var state = {};
+
 
 function action(element){
-	var showed = element.attr("showed");
-	console.log(showed);
-	var anti = !showed;
-	console.log(anti);
-	element.attr("showed", anti);
-	console.log(element.attr("showed"));
-	if (showed === true){
+	var id = element[0].id;
+	var showed = state[id];
+	if (showed){
 		element.hide();	
 	} else {
 		element.show();
 	}
+	state[id] = !state[id];
 };
 
 $(document).ready(function(){
 	$("h3").next().hide();
 	$("h3").next().attr("showed", false);
+	$("h3").next().each(function(index, value){
+		state[value.id] = false;
+	});
 	$("h3").click(function(){ action($(this).next()); });
 });
+
+console.log('Completed');
+
+// function action(element){
+// 	var showed = element.attr("showed");
+// 	console.log(showed);
+// 	var anti = !showed;
+// 	console.log(anti);
+// 	element.attr("showed", anti);
+// 	console.log(element.attr("showed"));
+// 	if (showed === true){
+// 		element.hide();	
+// 	} else {
+// 		element.show();
+// 	}
+// };
+
+// $(document).ready(function(){
+// 	$("h3").next().hide();
+// 	$("h3").next().attr("showed", false);
+// 	$("h3").click(function(){ action($(this).next()); });
+// });
