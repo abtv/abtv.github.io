d04206b9329169cbddbadf1deb151d6b4af0ab0a
Andrey Butov <andreybutov@mail.ru>
2014-07-17 17:22:44 +0400

    Menu was implemented in javascript

---
+++ javascripts/menu.js
@@ -0,0 +1,41 @@
+function createMenu(currentPage){
+  var navbar = $('<div />').appendTo('body');
+  navbar.attr("class", "navbar-inverse navbar-fixed-top");
+  navbar.attr("role", "navigation");
+
+    var container = $('<div />').appendTo(navbar);
+    container.attr("class", "container");
+
+      var navbarHeader = $('<div />').appendTo(container);
+      navbarHeader.attr("class", "navbar-header");
+
+        var navbarBrand = $('<div />').appendTo(container);
+        navbarBrand.attr("class", "navbar-brand");
+        navbarBrand.text("abtv's blog");
+
+      var navbarCollapse = $('<div />').appendTo(container);
+      navbarCollapse.attr("class", "collapse navbar-collapse");
+
+        var navbarNav = $('<ul />').appendTo(navbarCollapse);
+        navbarNav.attr("class", "nav navbar-nav");
+
+          var liIndex = createMenuItem("index.html", "Technical blog", currentPage);
+          liIndex.appendTo(navbarNav);
+          var liEnglish = createMenuItem("english.html", "English blog", currentPage);
+          liEnglish.appendTo(navbarNav);
+          var liTrainer = createMenuItem("trainer.html", "My personal English trainer", currentPage);
+          liTrainer.appendTo(navbarNav);
+  $('<br />').appendTo('navbar');
+  $('<br />').appendTo('navbar');
+}
+
+function createMenuItem(href, title, currentPage){
+  var li = $('<li />');
+  if (href == currentPage){
+    li.attr("class", "active");
+  }
+    var a = $('<a />').appendTo(li);
+    a.attr("href", href);
+    a.text(title);
+  return li;
+}
